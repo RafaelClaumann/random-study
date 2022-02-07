a280f9dfd6551a84912309b212566bf06d4543fe
RafaelClaumann <rafael_claumann@hotmail.com>
2022-02-07 20:52:28 -0300

    move RestTemplateBeanConfig para package requests/rest_template/spring

--- java/requests/src/main/java/com/requests/rest_template/spring/RestTemplateBeanConfiguration.java
+++ java/requests/src/main/java/com/requests/rest_template/spring/RestTemplateBeanConfiguration.java
@@ -1,4 +1,4 @@
-package com.requests.spring;
+package com.requests.rest_template.spring;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
