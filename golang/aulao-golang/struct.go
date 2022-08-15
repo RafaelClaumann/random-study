5f92fd36d8c0697dd720d9542cd91a50da86f315
RafaelClaumann <rafael_claumann@hotmail.com>
2022-08-15 20:55:22 -0300

    mais ponteiros e struct golang

---
+++ golang/aulao-golang/struct.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Car struct {
+	model string
+	year  int32
+}
+
+func main() {
+	var carro Car = Car{"fusca", 1973}
+	changeYearPointer(&carro, 1974)
+	fmt.Printf("resultado: %v\n", carro)
+
+	var carro02 Car = Car{"fusca", 1994}
+	changeYearCopy(carro02, 1995)
+	fmt.Printf("resultado: %v\n", carro02) // {fusca 1994}
+}
+
+func changeYearPointer(car *Car, yearParam int32) {
+	car.year = yearParam
+}
+
+func changeYearCopy(car Car, yearParam int32) {
+	car.year = yearParam
+}
