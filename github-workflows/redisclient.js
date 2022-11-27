9bebf6b0ac2e98b4350a43f81818ebaebf21d68e
RafaelClaumann <rafael_claumann@hotmail.com>
2022-11-27 20:49:13 -0300

    update workflow-06.yaml

--- github-workflows/redisclient.js
+++ github-workflows/redisclient.js
@@ -1,49 +1,13 @@
-const redis = require("redis");
+import { createClient } from 'redis';
 
-// Creates a new Redis client
-// In the workflow we are going ot set If REDIS_HOST and REDIS_PORT
-const redisClient = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT
-});
+const client = createClient();
 
-console.log("REDIS_HOST", process.env.REDIS_HOST);
-console.log("REDIS_PORT", process.env.REDIS_PORT);
+client.on('error', (err) => console.log('Redis Client Error', err));
 
-redisClient.on("error", function (err) {
-  console.log("Error " + err);
-});
+await client.connect();
 
-redisClient.connect();
+await client.set('key', 'value');
+const value = await client.get('key');
+await client.disconnect();
 
-redisClient.set("hello", "world", redis.print);
-redisClient.set("spanish", "red", "rojo", redis.print);
-redisClient.set("spanish", "orange", "naranja", redis.print);
-redisClient.set("spanish", "blue", "azul", redis.print);
-redisClient.set("german", "red", "rot", redis.print);
-redisClient.set("german", "orange", "orange", redis.print);
-redisClient.set("german", "blue", "blau", redis.print);
-
-redisClient.get("hello", (err, value) => {
-  if (err) console.log(err);
-  else console.log(value);
-});
-
-redisClient.get("spanish", "red", (err, value) => {
-  if (err) console.log(err);
-  else console.log(value);
-});
-
-redisClient.keys("german", function (err, germankeys) {
-  console.log(germankeys.length + " germanWords:");
-  germankeys.forEach(function (germankey, i) {
-    redisClient.hget("spanish", germankey, (err, value) => {
-      if (err) console.log(err);
-      else
-        console.log(
-          "    " + i + " German word for: " + germankey + " is: " + value
-        );
-    });
-  });
-  redisClient.quit();
-});
+console.log(value);
