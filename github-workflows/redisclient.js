37537db64c02cbecd356427d56f52c61a767f03a
RafaelClaumann <rafael_claumann@hotmail.com>
2022-11-27 20:33:19 -0300

    arrumando javascript nojento

--- github-workflows/redisclient.js
+++ github-workflows/redisclient.js
@@ -3,39 +3,44 @@ const redis = require("redis");
 // Creates a new Redis client
 // In the workflow we are going ot set If REDIS_HOST and REDIS_PORT
 const redisClient = redis.createClient({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT
 });
 
 redisClient.on("error", function (err) {
-    console.log("Error " + err);
+  console.log("Error " + err);
 });
 
-redisClient.set('hello', 'world', redis.print);
-redisClient.hset('spanish', 'red', 'rojo', redis.print);
-redisClient.hset('spanish', 'orange', 'naranja', redis.print);
-redisClient.hset('spanish', 'blue', 'azul', redis.print);
-redisClient.hset('german', 'red', 'rot', redis.print);
-redisClient.hset('german', 'orange', 'orange', redis.print);
-redisClient.hset('german', 'blue', 'blau', redis.print);
+redisClient.connect();
 
-redisClient.get('hello', (err, value) => {
-    if (err) console.log(err);
-    else console.log(value);
+redisClient.set("hello", "world", redis.print);
+redisClient.set("spanish", "red", "rojo", redis.print);
+redisClient.set("spanish", "orange", "naranja", redis.print);
+redisClient.set("spanish", "blue", "azul", redis.print);
+redisClient.set("german", "red", "rot", redis.print);
+redisClient.set("german", "orange", "orange", redis.print);
+redisClient.set("german", "blue", "blau", redis.print);
+
+redisClient.get("hello", (err, value) => {
+  if (err) console.log(err);
+  else console.log(value);
 });
 
-redisClient.hget('spanish', 'red', (err, value) => {
-    if (err) console.log(err);
-    else console.log(value);
+redisClient.get("spanish", "red", (err, value) => {
+  if (err) console.log(err);
+  else console.log(value);
 });
 
-redisClient.hkeys("german", function (err, germankeys) {
-    console.log(germankeys.length + " germanWords:");
-    germankeys.forEach(function (germankey, i) {
-        redisClient.hget('spanish', germankey, (err, value) => {
-            if (err) console.log(err);
-            else console.log("    " + i + " German word for: " + germankey + " is: " + value)
-        });
+redisClient.keys("german", function (err, germankeys) {
+  console.log(germankeys.length + " germanWords:");
+  germankeys.forEach(function (germankey, i) {
+    redisClient.hget("spanish", germankey, (err, value) => {
+      if (err) console.log(err);
+      else
+        console.log(
+          "    " + i + " German word for: " + germankey + " is: " + value
+        );
     });
-    redisClient.quit();
+  });
+  redisClient.quit();
 });
