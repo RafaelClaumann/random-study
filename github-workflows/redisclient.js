0553ac0bfeb7e3e0d072723d120401d18847c40f
RafaelClaumann <rafael_claumann@hotmail.com>
2022-11-27 21:01:47 -0300

    update redisclient.js

--- github-workflows/redisclient.js
+++ github-workflows/redisclient.js
@@ -1,14 +1,17 @@
 const redis = require("redis");
 
-// Creates a new Redis client
-// In the workflow we are going ot set If REDIS_HOST and REDIS_PORT
-const redisClient = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT
-});
+const client = redis.createClient({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT
+  });
 
 console.log("REDIS_HOST", process.env.REDIS_HOST);
 console.log("REDIS_PORT, ", process.env.REDIS_PORT);
 
-redisClient.connect();
-redisClient.disconnect();
+client.on('error', (err) => console.log('Redis Client Error', err));
+
+await client.connect();
+
+await client.set('key', 'value');
+const value = await client.get('key');
+await client.disconnect();
