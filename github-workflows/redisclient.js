f3c20c2c676e2612cec1e57f11ced718f70d487b
RafaelClaumann <rafael_claumann@hotmail.com>
2022-11-27 21:10:59 -0300

    update redisclient.js

--- github-workflows/redisclient.js
+++ github-workflows/redisclient.js
@@ -1,34 +1,19 @@
-const redis = require("redis");
 
-const client = redis.createClient({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT
-  });
+var redis = require("redis");
 
-console.log("REDIS_HOST", process.env.REDIS_HOST);
-console.log("REDIS_PORT, ", process.env.REDIS_PORT);
+async function testCache() {
+    var client = redis.createClient({
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT
+    });
+    
+    await client.connect();
 
-client.on('error', (err) => console.log('Redis Client Error', err));
+    console.log("\nCache command: PING");
+    console.log("Cache response : " + await client.ping());
 
-async function connect() {
-    client.connect();
+    console.log("\nDone");
+    process.exit();
 }
 
-async function setKey() {
-    client.set('key', 'value');
-}
-
-let value = NaN;
-async function retrieveKey() {
-    value = client.get('key');
-    console.log(value);
-}
-
-async function disconnect() {
-    client.disconnect();
-}
-
-await connect();
-await setKey();
-await retrieveKey();
-await disconnect();
+testCache();
