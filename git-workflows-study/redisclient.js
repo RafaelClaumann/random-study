1df954ab18691aa67cad6409feb96e595ae3f9a9
RafaelClaumann <rafael_claumann@hotmail.com>
2022-11-29 20:18:25 -0300

    update redisclient

--- git-workflows-study/redisclient.js
+++ git-workflows-study/redisclient.js
@@ -6,8 +6,8 @@ console.log(process.env.REDIS_PORT);
 
 async function testCache() {
     var client = redis.createClient({
-        host: process.env.REDIS_HOST,
-        port: process.env.REDIS_PORT
+        host: "redis_client",
+        port: 6379
     });
 
     await client.connect();
