7fa9837c719f8ff6bc2c717bd2c89f10d386e12e
RafaelClaumann <rafael_claumann@hotmail.com>
2022-11-29 22:13:06 -0300

    update redisclient & workflow-06

--- git-workflows-study/redisclient.js
+++ git-workflows-study/redisclient.js
@@ -1,13 +1,23 @@
-const redis = require('redis');
+const redis = require("redis");
+
+console.log(process.env.REDIS_HOST);
+console.log(process.env.REDIS_PORT);
+
 const client = redis.createClient({
     socket: {
-        host: 'redis_service',
-        port: '6379'
-    }
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT
+    },
+    password: "redis2022"
 });
 
-client.on('error', err => {
-    console.log('Error ' + err);
-});
+client.on("error", err => { console.log("Error " + err); });
 
-client.connect();
+async function testCache() {
+    client.connect();
+    console.log("\nCache command: PING");
+    console.log("Cache response: " + await client.ping());
+    client.disconnect();
+}
+
+testCache();
