c30007aba39afa0ca8448cf574cac96aaf7c00f0
RafaelClaumann <rafael_claumann@hotmail.com>
2022-12-01 11:02:46 -0300

    wkfl - workflow-06.yaml - redis ServiceContainer

---
+++ git-workflows/redisclient.js
@@ -0,0 +1,22 @@
+const redis = require("redis");
+
+console.log(process.env.REDIS_HOST);
+console.log(process.env.REDIS_PORT);
+
+const client = redis.createClient({
+    socket: {
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT
+    }
+});
+
+client.on("error", err => { console.log("Error " + err); });
+
+async function testCache() {
+    client.connect();
+    console.log("\nCache command: PING");
+    console.log("Cache response: " + await client.ping());
+    client.disconnect();
+}
+
+testCache();
