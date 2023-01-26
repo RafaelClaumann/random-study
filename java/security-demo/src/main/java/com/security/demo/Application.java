391693b3dcca9bbfdbc818b6ee9c56a44c468cba
RafaelClaumann <rafael_claumann@hotmail.com>
2023-01-26 19:46:17 -0300

    security - criando model e repository para User

--- java/security-demo/src/main/java/com/security/demo/Application.java
+++ java/security-demo/src/main/java/com/security/demo/Application.java
@@ -1,7 +1,9 @@
 package com.security.demo;
 
 import com.security.demo.model.Post;
+import com.security.demo.model.User;
 import com.security.demo.repository.PostRepository;
+import com.security.demo.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,9 +17,11 @@ public class Application {
     }
 
     @Bean
-    CommandLineRunner runSomething(final PostRepository postRepository) {
+    CommandLineRunner runSomething(final PostRepository postRepository, final UserRepository userRepository) {
         return args -> {
             postRepository.save(new Post("Hello World", "hello-world", "Welcome to my blog!", "Rafael"));
+            userRepository.save(new User("rafael", "123", "ROLE_USER"));
+            userRepository.save(new User("admin", "123", "ROLE_USER,ROLE_ADMIN"));
         };
     }
 
---
+++ java/security-demo/src/main/java/com/security/demo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.security.demo.repository;
+
+import com.security.demo.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+}
