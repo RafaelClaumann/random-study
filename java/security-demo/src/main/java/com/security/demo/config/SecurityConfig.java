b170dfee5c3028b1e9f7a70faf0bf334cf77b0de
RafaelClaumann <rafael_claumann@hotmail.com>
2023-01-26 13:57:09 -0300

    security - criando bean SecurityFilterChain
    SecurityConfig
      As versoes mais recentes do spring usam um bean do tipo
    SecurityFilterChain para sobrescrever as configuracoes de segurança
    default que sao entregues pelo SpringBoot.
      Em versoes anteriores a classe WebSecurityConfigurerAdapter era
    estendida e o metodo configure(HttpSecurity http) era sobrescrito.
    
    As linhas:
      csrf(csrf -> csrf.ignoringAntMatchers("/h2-console/**"))
      antMatchers("/h2-console/**").permitAll()
      headers(headers -> headers.frameOptions().sameOrigin())
    Sao usadas para permitir o acesso ao console do H2 sem precisar
    se autenticar no Spring Security

---
+++ java/security-demo/src/main/java/com/security/demo/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.security.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.ignoringAntMatchers("/h2-console/**"))
+                .authorizeRequests(auth -> auth
+                        .antMatchers("/h2-console/**").permitAll()
+                        .mvcMatchers("/api/posts").permitAll()
+                        .anyRequest().authenticated())
+                .headers(headers -> headers.frameOptions().sameOrigin())
+                .build();
+    }
+
+}
