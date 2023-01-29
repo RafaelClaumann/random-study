8ee67f501801151d1bc902a39d1f12d16d9ae79b
RafaelClaumann <rafael_claumann@hotmail.com>
2023-01-28 22:50:21 -0300

    security2 - usuario em memoria e URLs com acesso por ROLE
    SecurityConfig
      Criando usuarios e suas ROLES.
      Acesso as URLs apenas para usuarios que possuem determinada ROLE.
    
    application.properties
      Excluindo as propriedades relacionadas a criação do usuario
      default do Spring Security.
      " The default auto configuration provides an InMemoryUserDetailsManager that generates a single
        user for the application to support. We can override these user properties to an extent with
        changes to application.properties file. If we wanted multiple users within our servers, we
        need to provide our implementation of an UserDetailsService. "
        https://springhow.com/in-memory-userdetailsservice-in-spring-security/

--- java/security-demo-two/src/main/java/com/security/config/SecurityConfig.java
+++ java/security-demo-two/src/main/java/com/security/config/SecurityConfig.java
@@ -1,9 +1,16 @@
 package com.security.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -12,13 +19,36 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.authorizeHttpRequests(authConfig -> {
-                    authConfig.antMatchers("/").permitAll();
-                    authConfig.antMatchers("/user/**").authenticated();
-                    authConfig.antMatchers("/admin/**").denyAll();
+                    authConfig.antMatchers(HttpMethod.GET, "/").permitAll();
+                    authConfig.antMatchers(HttpMethod.GET, "/user/**").hasAnyRole("ADMIN", "USER");
+                    authConfig.antMatchers(HttpMethod.GET, "/admin/**").hasRole("ADMIN");
+                    authConfig.anyRequest().hasRole("ADMIN");
                 })
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults())
                 .build();
     }
-    
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("password")
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user = User.builder()
+                .username("rafael")
+                .password("123")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
 }
