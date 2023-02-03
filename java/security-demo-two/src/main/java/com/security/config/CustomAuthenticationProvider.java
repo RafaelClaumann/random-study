e6f3f70498f1ae806bb53358709e02b312a655d6
RafaelClaumann <rafael_claumann@hotmail.com>
2023-02-02 21:02:12 -0300

    security2 - custom AuthenticationProvider

---
+++ java/security-demo-two/src/main/java/com/security/config/CustomAuthenticationProvider.java
@@ -0,0 +1,43 @@
+package com.security.config;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    private final UserDetailsService userDetailsService;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public CustomAuthenticationProvider(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        final String username = authentication.getName();
+        final String password = authentication.getCredentials().toString();
+        final UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+
+
+        if (this.passwordEncoder.matches(password, userDetails.getPassword()))
+            return new UsernamePasswordAuthenticationToken(username, password, userDetails.getAuthorities());
+
+        throw new BadCredentialsException("Something went wrong with authentication");
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+
+}
