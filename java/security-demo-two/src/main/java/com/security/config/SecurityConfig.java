36ba96a46f81203d19f5823ab6086b25915002b9
RafaelClaumann <rafael_claumann@hotmail.com>
2023-02-14 19:55:20 -0300

    security2 - ajuste formLogin() no filtro HttpSecurity

--- java/security-demo-two/src/main/java/com/security/config/SecurityConfig.java
+++ java/security-demo-two/src/main/java/com/security/config/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authConfig -> authConfig
                         .mvcMatchers("/", "login").permitAll()
                 )
+                .formLogin(Customizer.withDefaults())
                 .build();
     }
 
