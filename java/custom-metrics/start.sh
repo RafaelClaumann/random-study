e6750bf31066ba7f01c69fe4587b07aa3ee59c35
RafaelClaumann <rafael_claumann@hotmail.com>
2024-03-10 00:08:05 -0300

    rename restart.sh -> start.sh

--- java/custom-metrics/start.sh
+++ java/custom-metrics/start.sh
@@ -3,3 +3,6 @@ docker image rm -f demoapp:1.0 2>/dev/null
 mvn clean install -Dmaven.test.skip --threads 4C
 docker image build -t demoapp:1.0 .
 docker-compose up -d
+
+read -r -p "Press any key to destroy containers..." key
+docker-compose down --volumes
