6a5263927c31dd710b4f56a6eff6f5a9e0b2101e
RafaelClaumann <rafael_claumann@hotmail.com>
2024-03-19 22:55:56 -0300

    move docker related files to a folder

--- java/custom-metrics/docker/start.sh
+++ java/custom-metrics/docker/start.sh
@@ -1,7 +1,11 @@
 docker-compose down --volumes
 docker image rm -f demoapp:1.0 2>/dev/null
+
+cd ..
 mvn clean install -Dmaven.test.skip --threads 4C
-docker image build -t demoapp:1.0 .
+docker image build -f docker/Dockerfile -t demoapp:1.0 .
+cd docker/
+
 docker-compose up -d
 
 read -r -p "Press any key to destroy containers..." key
